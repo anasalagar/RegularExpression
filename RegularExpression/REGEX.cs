d93501983a553f52debdd50d41096a39975798dd
anas salagar <alisalagarf@gmail.com>
2021-11-21 20:09:50 +0530

    [anas UC7 validation of password whsich contains only 8 charactors]

--- RegularExpression/REGEX.cs
+++ RegularExpression/REGEX.cs
@@ -44,6 +44,70 @@ namespace RegularExpression00
                 Console.WriteLine(name + " is invalid last name. Please start with capital letter and take maximums 3 charactors");
             }
         }
+        //This method for validation of email id
+        public static void EmailValid()
+        {
+            //taking input email id from user
+            Console.Write("Email id : ");
+            string name = Console.ReadLine();
+            //Regular expression of email id
+            string emailid = "^[a-z]{3}[.][a-z]*[@]{1}[bl]{2}[.]{1}[co]{2}[.]{1}[a-z]*$";
+            Regex regex = new Regex(emailid);
+
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine("Email Id is valid");
+            }
+            else
+            {
+                Console.WriteLine("Email Is Invalid,Please Enter Valid Email Id.");
+            }
+
+        }
+        //This method for validation of mobile number
+        public static void MobileNumValid()
+        {
+            //taking input mobile number from user
+            Console.WriteLine("Format of mobile number : 00 0000000000");
+            Console.Write("Enter Mobile Number : ");
+            string num = (Console.ReadLine());
+            //Regular expression of mobile number
+            string number = "^[0-9]{1,2}[ ]{1}[0-9]{10}$";
+            Regex regex = new Regex(number);
+
+            if (regex.IsMatch(num))
+            {
+                Console.WriteLine("Mobile Number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Mobile number is Invalid,Please Enter Valid MObile Number format.");
+            }
+
+        }
+        //This method for validation password
+        public static void PasswordValid()
+        {
+            //taking input password from user
+            Console.Write("Enter Password : ");
+            string name = Console.ReadLine();
+            //Regular expression of password
+            //Rule 1 : minimum 8 charactors
+            //Rule 2 : atleast 1 uppercase
+            //Rule 3 : atleast 1 numeric number
+            string password = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$";
+            Regex regex = new Regex(password);
+
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                Console.WriteLine("Password Is Invalid,Please Enter Valid Email Id.");
+            }
+
+        }
     }
-    
+
 }
