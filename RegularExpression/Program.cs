0b21a4cfbb2bf9e34355e3ebb41054b807aab119
anas salagar <alisalagarf@gmail.com>
2021-11-21 19:17:22 +0530

    [anas UC1 for validating first name]

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RegularExpression00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression Program");
+            Console.WriteLine();
+            //taking user input of first name
+            Console.Write("Enter First Name : ");
+            string firstname = Console.ReadLine();
+            RegularExpression.FirstName(firstname);
+            Console.ReadLine();
+
+        }
+    }
+}
