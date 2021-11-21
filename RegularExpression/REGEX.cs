68c2092b6f19dda0b6f33e5d461bd7d10b35e93e
anas salagar <alisalagarf@gmail.com>
2021-11-21 19:35:22 +0530

    [anas UC2 for last name]

--- RegularExpression/REGEX.cs
+++ RegularExpression/REGEX.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace RegularExpression
+namespace RegularExpression00
 {
-    class REGEX
+    public class RegularExpression
     {
+        //This method for matching pattern of first name
+        public static void FirstNameValid(string name)
+        {
+            //Regular expression for first letter is capital and maximum charactor is 3
+            string firstname = "^[A-Z]{1}[a-zA-Z]{2}$";
+            Regex regex = new Regex(firstname);
+            //if condition for matching pattern
+            //if condition is true then if block will execute
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            //else condition for if condition is false then else block will execute
+            else
+            {
+                Console.WriteLine(name + " is invalid first name. Please start with capital letter and only take maximum 3 charactors");
+            }
+        }
+        //This method for validate last name
+        public static void LastNameNameValid(string name)
+        {
+            //Regular expression for first letter is capital and maximum charactor is 3
+            string firstname = "^[A-Z]{1}[a-zA-Z]{2}$";
+            Regex regex = new Regex(firstname);
+            //if condition for matching pattern
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            //else condition for if condition is false then else block will execute
+            else
+            {
+                Console.WriteLine(name + " is invalid last name. Please start with capital letter and take maximums 3 charactors");
+            }
+        }
     }
+    
 }
