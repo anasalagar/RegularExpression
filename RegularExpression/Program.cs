f622e2c55ef85e472f113ad28bcfdc44d9fe7e19
anas salagar <alisalagarf@gmail.com>
2021-11-21 19:29:34 +0530

    [anas UC3 for validation of Email ID]

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -8,10 +8,17 @@ namespace RegularExpression00
         {
             Console.WriteLine("Welcome to Regular Expression Program");
             Console.WriteLine();
-            //taking user input of first name
-            Console.Write("Enter First Name : ");
-            string firstname = Console.ReadLine();
-            RegularExpression.FirstName(firstname);
+            //taking user input and passing to FirstNameValid method
+            //Console.Write("Enter First Name : ");
+            ////string firstname = Console.ReadLine();
+            //RegularExpression.FirstNameValid(firstname);
+            //taking user input and passing to LastNameValid method
+            //Console.Write("\nEnter Last Name : ");
+            //string lastName = Console.ReadLine();
+            //RegularExpression.LastNameNameValid(lastName);
+
+            RegularExpression.EmailValid();
+
             Console.ReadLine();
 
         }
